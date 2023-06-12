9ea611b2136edca9a4ff3a5f7f293274a84b7611
Jasna Robić Borovac <jrborovac@infodom.hr>
2023-06-12 13:57:23 +0200

    Dodana tablica Glumci

---
+++ app/Models/Glumci.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace app.Models
+{
+    public class Glumci
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Ime { get; set; }
+
+        [Required]
+        public string Prezime { get; set; }
+    }
+}
